942b92070441fc8612e236d105c568232d01230c
mdallen5393 <allen_matthew@outlook.com>
2023-01-17 08:36:28 -0600

    Task 0. Merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,128 +1,102 @@
 #include "sort.h"
 
 /**
- * merge_sort - Implementation of top-down merge sort algorithm
- * @array: pointer to array of integers to be sorted
- * @size: size of given array
+ * merge_sort - sorts an array of integers in ascending order
+ * using merge sort algorithm
  *
- * Return: void
+ * @array: array of integers to sort
+ * @size: size of array to be sorted
  */
-
 void merge_sort(int *array, size_t size)
 {
-	int *temp_array = malloc(sizeof(array[0]) * size);
-	/* print_my_array(array, 0, size / 2); */
-	copy_array(array, 0, size, temp_array);
-	/* print_my_array(temp_array, 0, size); */
-	recur_split(temp_array, 0, size, array);
-	free(temp_array);
+	int len = (int)size, i;
+	int *A = array, *B;
+
+	/* Make a copy of input array A into array B */
+	B = malloc(sizeof(int) * len);
+	for (i = 0; i < len; i++)
+		B[i] = A[i];
+
+	split(B, A, 0, len);
+	free(B);
 }
 
 /**
- * print_my_array - Prints an array between two indices
- * @array: Array to print
- * @start: Starting index of array to print
- * @end: Ending index of array to print
+ * split - recursively splits an array into trivially sorted, single-member
+ * subarrays.
  *
- * Return: void
+ * @A: first array
+ * @B: temporary array
+ * @left: left index of subarray
+ * @right: right index of subarray
  */
-
-void print_my_array(int *array, int start, int end)
+void split(int *A, int *B, int left, int right)
 {
-	int i;
+	int mid;
 
-	/* printf("Printed with print_my_array:\n"); */
-
-	for (i = start; i < end; i++)
-	{
-		printf("%d", array[i]);
-		if (i < end - 1)
-		{
-			printf(", ");
-		}
-		else
-			printf("\n");
-	}
-}
-
-/**
- * copy_array - Copies from array to temp_array
- * @array: Original Array
- * @start_index: Starting index to copy
- * @end_index: End index to copy
- * @temp_array: Holds the copied array
- *
- * Return: void
- */
-void copy_array(int *array, int start_index, int end_index, int *temp_array)
-{
-	int i;
-
-	for (i = start_index; i < end_index; i++)
-		temp_array[i] = array[i];
-}
-
-/**
- * recur_split - Splits the array recursively until it is of size 1
- * @temp_array: Temporary array, copy of given array
- * @array: Given array to sort
- * @start: Starting index of array to split
- * @end: End index of array to split
- *
- * Return: Void
- */
-void recur_split(int *temp_array, int start, int end, int *array)
-{
-	int mid = (end + start) / 2;
-
-	if (end - start <= 1) /*Gets out if array has length of 1*/
-	{
-		/* print_my_array(array, start, end); */
+	if (right - left <= 1)
 		return;
-	}
 
-	recur_split(array, start, mid, temp_array);
-	recur_split(array, mid, end, temp_array);
+	mid = (left + right) / 2;
 
-	time_to_merge(temp_array, start, mid, end, array);
-}
+	/* recursively split */
+	split(B, A, left, mid);
+	split(B, A, mid, right);
 
-/**
- * time_to_merge - Takes all of the split arrays, compares and merges
- * @array: Given array, one element long to start
- * @start: starting index of array
- * @mid: middle index of array
- * @end: ending index of rarry
- * @temp_array: Array to be returned to print
- *
- * Return: Void
- */
-void time_to_merge(int *array, int start, int mid, int end, int *temp_array)
-{
-	int i = start; /*Used to index through left run*/
-	int j = mid; /*Used to index through right run*/
-
-	/*Used to index through temp_array as left and right runs are sorted*/
-	int k = start;
-
-	for (k = start; k < end; k++)
-	{
-		if (i < mid && (j >= end || array[i] <= array[j]))
-		{
-			temp_array[k] = array[i];
-			i++;
-		}
-		else
-		{
-			temp_array[k] = array[j];
-			j++;
-		}
-	}
+	/* merge back together */
+	merge(A, B, left, mid, right);
 
 	printf("Merging...\n[left]: ");
-	print_my_array(array, start, mid);
+	print_subarray(A, left, mid);
 	printf("[right]: ");
-	print_my_array(array, mid, end);
+	print_subarray(A, mid, right);
 	printf("[Done]: ");
-	print_my_array(temp_array, start, end);
+	print_subarray(B, left, right);
+}
+
+/**
+ * merge - merges two sorted arrays
+ *
+ * @A: first array to merge
+ * @B: second array to merge
+ * @left: start of first subarray
+ * @mid: start of right subarray
+ * @right: end of array
+ */
+void merge(int *A, int *B, int left, int mid, int right)
+{
+	int i, aPos, bPos;
+
+	aPos = left;
+	bPos = mid;
+
+	for (i = left; i < right; i++)
+	{
+		if (aPos < mid && (bPos >= right || A[aPos] <= A[bPos]))
+		{
+			B[i] = A[aPos];
+			aPos++;
+		}
+		else
+		{
+			B[i] = A[bPos];
+			bPos++;
+		}
+	}
+}
+
+/**
+ * print_subarray - prints a subarray given start and end
+ *
+ * @arr: array containing subarray to print
+ * @start: first index to print
+ * @end: index to print to
+ */
+void print_subarray(int *arr, int start, int end)
+{
+	int i;
+
+	for (i = start; i < end - 1; i++)
+		printf("%d, ", arr[i]);
+	printf("%d\n", arr[i]);
 }
